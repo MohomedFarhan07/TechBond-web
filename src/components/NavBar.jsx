ecbbf98e4eb095c84428177fe407f9f177a35089
MohomedFarhan07 <offxl.mohofarhan07@gmail.com>
2026-02-20 16:56:57 +0530

    feat: Setup the routing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">👨‍💻 Techbond</a>
       </div>
