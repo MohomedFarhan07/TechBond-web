4e113c852b0ccf868cfe2525f88e533e57ee706b
MohomedFarhan07 <offxl.mohofarhan07@gmail.com>
2026-02-24 21:54:55 +0530

    feat: Connected the chat page to the database

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 const Chat = () => {
   const { targetUserId } = useParams();
@@ -16,9 +18,8 @@ const Chat = () => {
     const socket = createSocketConnection();
     socket.emit("joinChat", { userId, targetUserId });
 
-    socket.on("messageReceived", ({ firstName, text }) => {
-      setMessages((messages) => [...messages, {firstName, text}]);
-      console.log(text);
+    socket.on("messageReceived", ({ firstName, lastName, text }) => {
+      setMessages((messages) => [...messages, { firstName, lastName, text }]);
     });
 
     return () => socket.disconnect();
@@ -28,6 +29,7 @@ const Chat = () => {
     const socket = createSocketConnection();
     socket.emit("sendMessage", {
       firstName: user.firstName,
+      lastName: user.lastName,
       userId,
       targetUserId,
       text: newMessage,
@@ -36,18 +38,45 @@ const Chat = () => {
     setNewMessage("");
   };
 
+  const fetchChatMessages = async () => {
+    const chat = await axios.get(`${BASE_URL}/chat/${targetUserId}`, {
+      withCredentials: true,
+    });
+
+    const chatMessages = chat?.data?.messages.map((msg) => {
+      return {
+        firstName: msg?.senderId?.firstName,
+        lastName: msg?.senderId?.lastName,
+        text: msg.text,
+      };
+    });
+
+    setMessages(chatMessages);
+  };
+
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
+
   return (
     <div className="w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
       <h1 className="p-5 border-b border-gray-600">Chat</h1>
       <div className="flex-1 overflow-scroll p-5">
-        {messages && messages.map((msg, i) => {
-          return (
-            <div key={i} className="chat chat-start">
-              <div className="chat-header">{msg.firstName}</div>
-              <div className="chat-bubble">{msg.text}</div>
-            </div>
-          );
-        })}
+        {messages &&
+          messages.map((msg, i) => {
+            return (
+              <div
+                key={i}
+                className={
+                  "chat " +
+                  (user.lastName == msg.lastName ? "chat-end" : "chat-start")
+                }
+              >
+                <div className="chat-header">{`${msg.firstName} ${msg.lastName}`}</div>
+                <div className="chat-bubble">{msg.text}</div>
+              </div>
+            );
+          })}
       </div>
       <div className="p-5 border-t border-gray-600 flex items-center gap-2">
         <input
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,14 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 const createSocketConnection = () => {
-  return io(BASE_URL);
+  if(location.hostname === "localhost"){
+    return io(BASE_URL);
+  } else {
+    return io("/", {path: "/api/socket.io"})
+  }
+
+    return io(BASE_URL);
+  
 };
 
 export { createSocketConnection };
